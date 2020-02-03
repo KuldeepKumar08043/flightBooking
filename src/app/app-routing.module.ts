47050acc7a1dbe17d3b4f0897b1e9d2650fbe4da
2388853 <2388853@AFRICA.AIRTEL.ITM>
2020-02-03 17:04:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookingDashboardComponent } from './container/booking-dashboard/booking-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './container/login/login.module#LoginModule'
+  },
+  {
+    path: 'booking-dashboard',
+    component: BookingDashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {MatSortModule} from '@angular/material/sort';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingDashboardComponent } from './container/booking-dashboard/booking-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BookingDashboardComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
